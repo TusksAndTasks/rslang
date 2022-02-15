059ed33e0f164903928c6c33b0143a7c19c2d7d7
Sweta <Stellla@tut.by>
2022-02-15 17:58:00 +0100

    feat: add page statistics for authoris. user

--- src/pages/statistics.ts
+++ src/pages/statistics.ts
@@ -28,11 +28,32 @@ export class Statistics {
   }
   private getHTMLFotAuth(): string {
     return /*html*/ `
-    <div class="statistics mt-10">
-    <h2> Cтатистика </h2>
-    <div class="statistics__icon"></div>
-    <p> s </p>
-
+    <div class="statistics-auth mt-10">
+      <h2> Cтатистика за ${new Date().toLocaleDateString()}</h2>
+      <div class="statistics-auth__icon"></div>
+      <div class="statistics-auth__wrapper">
+        <div class="statistics-auth__item">
+        <h2> Общая статистика </h2>
+        <p>Количество сыгранных игр</p>
+        <p>Количество новых слов</p>
+        <p>Процент правильных ответов</p>
+        <p> Самая длинная серия правильных ответов</p>
+        </div>
+        <div class="statistics-auth__item">
+        <h2> Игра Аудиовызов </h2>
+        <p>Количество сыгранных игр</p>
+        <p>Количество новых слов</p>
+        <p>Процент правильных ответов</p>
+        <p> Самая длинная серия правильных ответов</p>
+        </div>
+        <div class="statistics-auth__item">
+        <h2> Игра Спринт </h2>
+        <p>Количество сыгранных игр</p>
+        <p>Количество новых слов</p>
+        <p>Процент правильных ответов</p>
+        <p> Самая длинная серия правильных ответов</p>
+        </div>   
+      </div>
     </div>
     `;
   }
