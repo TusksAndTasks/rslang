d375642c9568424c798afd61b0ffa2a7f3b5a2bf
Sweta <Stellla@tut.by>
2022-02-18 12:26:31 +0100

    fix: fixed first page game sprint

--- src/games/sprint/sprint-difficulty.ts
+++ src/games/sprint/sprint-difficulty.ts
@@ -6,14 +6,17 @@ import { EPage } from "../../types/types";
 export class SprintDifficulty {
   public getHTML(): string {
     return `
+    <div class="sprint-diff mt-10">
       <h2 class="sprint-title">Мини-игра спринт</h2>
+      <div class="sprint-diff__icon"></div>
+      <p> Выберите сложность игры </p>
       <div class="sprint-difficulty">
-        <button class="sprint-difficulty__button" id="sd0">1</button>
-        <button class="sprint-difficulty__button" id="sd1">2</button>
-        <button class="sprint-difficulty__button" id="sd2">3</button>
-        <button class="sprint-difficulty__button" id="sd3">4</button>
-        <button class="sprint-difficulty__button" id="sd4">5</button>
-        <button class="sprint-difficulty__button" id="sd5">6</button>
+        <button class="btn btn-blue" id="sd0">1</button>
+        <button class="btn btn-blue" id="sd1">2</button>
+        <button class="btn btn-blue" id="sd2">3</button>
+        <button class="btn btn-blue" id="sd3">4</button>
+        <button class="btn btn-blue" id="sd4">5</button>
+        <button class="btn btn-blue" id="sd5">6</button>
       </div>
       `;
   }
@@ -23,11 +26,7 @@ export class SprintDifficulty {
       ".sprint-difficulty"
     ) as HTMLElement;
     buttonsContainer.addEventListener("click", (e) => {
-      if (
-        (e.target as HTMLElement).classList.contains(
-          "sprint-difficulty__button"
-        )
-      ) {
+      if ((e.target as HTMLElement).classList.contains("btn-blue")) {
         this.startSprintGame(
           +((e.target as HTMLElement).id.slice(2) as string)
         );
